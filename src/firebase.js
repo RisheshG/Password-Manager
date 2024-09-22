df42861f609bfa8f75983d8825fb82b18298e4a1
RisheshG <142112687+RisheshG@users.noreply.github.com>
2024-09-23 03:08:36 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,9 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-
-
-
+const firebaseConfig = {
+    apiKey: "AIzaSyAtNyOTGwf2DkJNUnwIh_fwak9nY9G4Lwk",
+    authDomain: "password-manager-11224.firebaseapp.com",
+    projectId: "password-manager-11224",
+    storageBucket: "password-manager-11224.appspot.com",
+    messagingSenderId: "333322722705",
+    appId: "1:333322722705:web:2a342e5a59870c1f430e30",
+    measurementId: "G-5Y31ME8SLT"
+};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
